7ed53a8b96e18d761df1fb6e6f9bd44bbb0a2a44
fawdlstty <f@fawdlstty.com>
2024-08-23 00:45:11 +0800

    fix compile in CI

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ use std::path::PathBuf;
 fn main() {
     let crate_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     let config = cbindgen::Config::from_file("cbindgen.toml").unwrap();
-    cbindgen::generate_with_config(&crate_dir, config)
-        .unwrap()
-        .write_to_file(crate_dir.join("include/oml/oml.h"));
+    _ = cbindgen::generate_with_config(&crate_dir, config)
+        .map(|p| p.write_to_file(crate_dir.join("include/oml/oml.h")));
 }
