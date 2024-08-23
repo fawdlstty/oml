ad7d77ed88f26de33d59389c013975dce168ae69
fawdlstty <f@fawdlstty.com>
2024-08-23 23:52:12 +0800

    move config file to build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,17 @@
 use std::path::PathBuf;
 
 fn main() {
-    let crate_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    let config = cbindgen::Config::from_file("cbindgen.toml").unwrap();
+    let crate_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let config = cbindgen::Config {
+        language: cbindgen::Language::C,
+        pragma_once: true,
+        ..Default::default()
+    };
     _ = cbindgen::generate_with_config(&crate_dir, config)
         .map(|p| p.write_to_file(crate_dir.join("include/oml/oml.h")));
+    _ = std::fs::remove_file(format!(
+        "target/package/{}-{}/Cargo.lock",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION")
+    ));
 }
